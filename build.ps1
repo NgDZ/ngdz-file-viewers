59b602832b37d59c6fa9a5c005ee2fb648fd2441
badre429@hotmail.com <badre429@hotmail.com>
2019-02-16 14:10:18 +0100

    feat(img+video)

--- build.ps1
+++ build.ps1
@@ -21,7 +21,18 @@ Copy-Item .\src-pdf\* .\dist\pdf\ -Recurse
 .\node_modules\.bin\uglifyjs .\src-pdf\viewer.js -o .\dist\pdf\viewer.js
 Copy-Item .\dist\pdf\viewer.html .\dist\pdf\index.html
 
-Copy-Item .\src-browser\* .\dist\
+Copy-Item .\src-browser\* .\dist\ -Recurse
 .\node_modules\.bin\uglifyjs .\dist\browser.js -o .\dist\browser.js
-# HLM
+
+# Image 
+Copy-Item .\node_modules\viewerjs\dist\viewer.min.css .\dist\img\
+Copy-Item .\node_modules\viewerjs\dist\viewer.min.js  .\dist\img\
+
+# Videos 
+Copy-Item .\node_modules\video.js\dist\video.min.js .\dist\video\
+Copy-Item .\node_modules\video.js\dist\video-js.min.css  .\dist\video\
+Copy-Item .\node_modules\video.js\dist\font\ .\dist\video\ -Recurse
+Copy-Item .\node_modules\video.js\dist\lang\ .\dist\video\ -Recurse
+Get-ChildItem .\dist\video\lang\ -Exclude ar.*,fr.*,en.* -Recurse | Remove-Item
+# cleanup
 Get-ChildItem .\dist\*.map -Recurse | Remove-Item
